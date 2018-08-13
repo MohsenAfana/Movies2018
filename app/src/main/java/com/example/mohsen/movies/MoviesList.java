82fa37df410fc31fe49e2385b513d8fca0a81468
alex <aliabuharb8530@gmail.com>
2018-08-13 22:58:56 +0300

    add moviesList activity

---
+++ app/src/main/java/com/example/mohsen/movies/MoviesList.java
@@ -0,0 +1,19 @@
+package com.example.mohsen.movies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class MoviesList extends AppCompatActivity {
+    RecyclerView mRecyle_moviesList;
+    RecyclerView.LayoutManager mLayoutManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies_list);
+
+
+    }
+}
