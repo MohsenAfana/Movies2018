51367a0bb8eba36c976188343b0f3310dc1b0e95
hammoud442 <mattrash@dev-any.com>
2018-08-13 16:22:41 +0300

    nav & splach & logo

--- app/src/main/java/com/example/mohsen/movies/MainActivity.java
+++ app/src/main/java/com/example/mohsen/movies/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.mohsen.movies;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
+        final Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(mContext,Home_Layout.class));
+                finish();
+            }
+        },3000);
     }
 }
