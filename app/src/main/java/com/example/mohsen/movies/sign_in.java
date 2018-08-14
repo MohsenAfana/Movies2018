1ca0093a9fac7dcccb68dad65aeb2bfb202e9a04
MohsenAfana <you@example.com>
2018-08-14 11:39:37 +0300

    Mohsen last Edit

---
+++ app/src/main/java/com/example/mohsen/movies/sign_in.java
@@ -0,0 +1,6 @@
+package com.example.mohsen.movies;
+
+import android.app.Activity;
+
+public class sign_in extends Activity {
+}
---
+++ app/src/main/java/com/example/mohsen/movies/sign_up.java
@@ -0,0 +1,12 @@
+package com.example.mohsen.movies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sign_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
